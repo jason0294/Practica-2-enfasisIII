5e83b60ed09f697f41203f01b8447d4787881f8b
santirogu <santiagorock000@gmail.com>
2015-05-29 23:43:45 -0500

    Agrego actualización con modulo de angular-translate para que la app tenga varios idiomas, en este caso solo tiene idioma ingles.

--- trabajo2/client/app/app.js
+++ trabajo2/client/app/app.js
@@ -5,11 +5,19 @@ angular.module('trabajo2App', [
   'ngResource',
   'ngSanitize',
   'ui.router',
+  'pascalprecht.translate',
   'ui.bootstrap'
 ])
-  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $translateProvider) {
     $urlRouterProvider
       .otherwise('/');
 
     $locationProvider.html5Mode(true);
+
+    $translateProvider.translations('en', {
+        TITLE:  'HOMECENTER MACHINES',
+        TABLE_TITLE:      'MACHINES'
+    });
+
+    $translateProvider.preferredLanguage('en');
   });
